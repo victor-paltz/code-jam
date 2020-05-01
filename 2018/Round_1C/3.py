4d969514ec17260aa781c2ea2911fef693a146ac
Victor Paltz <paltz.victor@free.fr>
2020-05-01 23:59:44 +0200

    move

---
+++ 2018/Round_1C/3.py
@@ -0,0 +1,15 @@
+
+from collections import Counter, defaultdict
+
+
+def readline():
+    return [int(x) for x in input().split()]
+
+
+T = int(input())
+
+for t in range(1, T+1):
+
+    N = readline()[0]
+
+    W = readline()
